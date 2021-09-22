cfb5eb1357e976e91bf2d81b6a36f46b79988bc8
Lara Cook <190218@virtualwindow.co.za>
2021-09-22 12:48:24 -0700

    Connect Firebase

--- refresh/refresh/refreshApp.swift
+++ refresh/refresh/refreshApp.swift
@@ -6,12 +6,26 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct refreshApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        
+        print("Firebase... ")
+        FirebaseApp.configure()
+        
+        return true
     }
 }
