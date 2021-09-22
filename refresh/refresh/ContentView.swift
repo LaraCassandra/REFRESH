d410d81c2399f965cb3f3030bfd85886facd3a55
Lara Cook <190218@virtualwindow.co.za>
2021-09-22 11:59:11 -0700

    Project setup

---
+++ refresh/refresh/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  refresh
+//
+//  Created by Lara Cook on 2021/09/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ refresh/refresh/refreshApp.swift
@@ -0,0 +1,17 @@
+//
+//  refreshApp.swift
+//  refresh
+//
+//  Created by Lara Cook on 2021/09/22.
+//
+
+import SwiftUI
+
+@main
+struct refreshApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
