41f4e0eb384ff557206554617b597e0ecad018f6
Lara Cook <190218@virtualwindow.co.za>
2021-10-14 14:26:59 -0700

    Begin new post functionality

--- refresh/refresh/Views/NewPostView.swift
+++ refresh/refresh/Views/NewPostView.swift
@@ -19,6 +19,41 @@ struct NewPostView: View {
     @State var showingAlert = false
     @State var errorTitle = "Oh No!"
     
+    // IMAGE HANDLING
+    @State var showingImagePicker = false
+    @State var sourceType: UIImagePickerController.SourceType = .photoLibrary
+        
+    func loadImage() {
+        guard let inputImage = pickedImage else { return }
+        pickedImage = inputImage
+        displayImage = Image(uiImage: inputImage)
+    }
+    
+    // SHEET HANDLING
+    @State var showingActionSheet = false
+    
+    // FUNCTIONALITY HANDLING
+    func errorCheck() -> String? {
+        if caption.trimmingCharacters(in: .whitespaces).isEmpty || pickedImage == nil
+            {
+            return "Please upload an image with a caption"
+        }
+        return nil
+    }
+    
+    func clear() {
+        self.displayImage = nil
+        self.caption = ""
+    }
+    
+    func uploadImage() {
+        if let error = errorCheck() {
+            self.errorMessage = error
+            self.showingAlert = true
+            return
+        }
+        self.clear()
+    }
     
     var body: some View {
         
@@ -33,11 +68,25 @@ struct NewPostView: View {
                     .frame(alignment: .leading)
                     .multilineTextAlignment(.leading)
                 
-                Image(systemName: "photo")
-                    .resizable()
-                    .scaledToFit()
-                    .foregroundColor(Color("Peach"))
-                    .padding(20)
+                if displayImage != nil {
+                    displayImage!.resizable()
+                        .scaledToFit()
+                        .frame(width: 250, height: 150, alignment: .center)
+                        .onTapGesture(perform: {
+                            self.showingImagePicker = true
+                        })
+                }
+                else {
+                    Image(systemName: "photo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200, alignment: .center)
+                        .padding(10)
+                        .foregroundColor(Color("Peach"))
+                        .onTapGesture(perform: {
+                            self.showingImagePicker = true
+                        })
+                }
                 
                 TextEditor(text: $caption)
                     .padding(20)
@@ -45,6 +94,7 @@ struct NewPostView: View {
                     .background(RoundedRectangle(cornerRadius: 10.0).stroke(Color("Peach")))
                 
                 Button(action: {
+                    uploadImage()
                     print("Clicked post button")
                 }, label: {
                     Text("POST!")
@@ -60,7 +110,23 @@ struct NewPostView: View {
                     Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("Try Again")))
                 })
                 
-            }.padding()
+            }
+            .padding()
+            .sheet(isPresented: $showingImagePicker, onDismiss: loadImage){
+                ImagePicker(pickedImage: $pickedImage)
+            }.actionSheet(isPresented: $showingActionSheet){
+                ActionSheet(title: Text(""), buttons: [
+                    .default(Text("Upload an Image")){
+                        self.showingImagePicker = true
+                        self.sourceType = .photoLibrary
+                    },
+                    .default(Text("Take a Picture")){
+                        self.showingImagePicker = true
+                        self.sourceType = .camera
+                    },
+                    .cancel()
+                ])
+            }
             
         }
         
